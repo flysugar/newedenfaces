840eedbc2dca9a6fc9f86a1b4a80e063ff854ed8
flysugar <l.flysugar@gmail.com>
2016-05-24 22:35:29 +0200

    commented out GA autotrack, removed from build scripts (bower, npm)

--- app/main.js
+++ app/main.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
 
-require('autotrack');
+// require('autotrack');
 
 // In the analytics.js tracking snippet
-ga('create', 'UA-78288609-1', 'auto');
-ga('require', 'autotrack');
-ga('send', 'pageview');
+// ga('create', 'UA-78288609-1', 'auto');
+// ga('require', 'autotrack');
+// ga('send', 'pageview');
 
 let history = createBrowserHistory();
 
