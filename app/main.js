472c460ab197e57a901bdd0d880df0215e16e979
flysugar <l.flysugar@gmail.com>
2016-05-24 22:20:24 +0200

    GA added

--- app/main.js
+++ app/main.js
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
 
+require('autotrack');
+
+// In the analytics.js tracking snippet
+ga('create', 'UA-78288609-1', 'auto');
+ga('require', 'autotrack');
+ga('send', 'pageview');
+
 let history = createBrowserHistory();
 
 ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('app'));
